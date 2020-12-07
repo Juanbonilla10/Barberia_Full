e8dd48cdff1a9677028bc6f36fe52e8995164127
Juan Carlos <coronadobonillajuancarlos904@gmail.com>
2020-12-06 22:50:05 -0500

    CAMBIOS POR ROL

--- Source Packages/edu/proyectofinal/controlador/CitaClienteView.java
+++ Source Packages/edu/proyectofinal/controlador/CitaClienteView.java
@@ -6,14 +6,20 @@
 package edu.proyectofinal.controlador;
 
 import edu.proyectofinal.facade.CitasFacadeLocal;
+import edu.proyectofinal.facade.ServiciosFacadeLocal;
+import edu.proyectofinal.facade.TipoPagoFacadeLocal;
+import edu.proyectofinal.facade.UsuariosFacadeLocal;
 import edu.proyectofinal.modelo.Citas;
+import edu.proyectofinal.modelo.Servicios;
+import edu.proyectofinal.modelo.TipoPago;
+import edu.proyectofinal.modelo.Usuarios;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.inject.Named;
-import javax.faces.view.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
 /**
@@ -21,44 +27,114 @@ import javax.inject.Inject;
  * @author soporte
  */
 @Named(value = "citaClienteView")
-@ViewScoped
-public class CitaClienteView implements Serializable{
-    
-    
+@SessionScoped
+public class CitaClienteView implements Serializable {
+
     @EJB
     CitasFacadeLocal citasFacadeLocal;
     private Citas citas = new Citas();
     private ArrayList<Citas> listacitas = new ArrayList<>();
-    
+
+    @EJB
+    TipoPagoFacadeLocal tipoPagoFacadeLocal;
+    private TipoPago tipoPago = new TipoPago();
+    private ArrayList<TipoPago> listapago = new ArrayList<>();
+
+    @EJB
+    UsuariosFacadeLocal usuariosFacadeLocal;
+    private Usuarios usuarios = new Usuarios();
+    private ArrayList<Usuarios> listausu = new ArrayList<>();
+
+    @EJB
+    ServiciosFacadeLocal serviciosFacadeLocal;
+    private Servicios servicios = new Servicios();
+    private ArrayList<Servicios> listaservices = new ArrayList<>();
+
     @Inject
     UsuarioSession usuarioSession;
-    
+
     @PostConstruct
     public void cargaCategorias() {
         try {
-            
+            listausu.addAll(usuariosFacadeLocal.findAll());
+            listapago.addAll(tipoPagoFacadeLocal.findAll());
+            listaservices.addAll(serviciosFacadeLocal.findAll());
             listacitas.addAll(citasFacadeLocal.listaCitasPer(usuarioSession.getCedula()));
+            citas.setServiciosIdservicio(new Servicios());
+            citas.setUsuariosidUsuarios(new Usuarios());
+            citas.setTipopagoidTipoPago(new TipoPago());
             System.out.println("CEDULA : " + usuarioSession.getCedula());
         } catch (Exception e) {
             System.out.println("edu.webapp1966781a.controlador.ProductosView.cargaCategorias() " + e.getMessage());
         }
     }
-    
+
     public void selecionCategoria(int idCitas) {
         citas = citasFacadeLocal.find(idCitas);
     }
-    
-     public int catidadProductos() {
+
+    public int catidadProductos() {
         return citasFacadeLocal.count();
     }
-     
-//     public List<Citas> listarPorCategoria() {
-//        return citasFacadeLocal.listaCitasPer(usuarioSession.getCedula());
-//    }
-//    
-    
 
-    
+    public void crearCitaCliente() {
+
+        try {
+
+            citas.setUsuariosidUsuarios(usuariosFacadeLocal.find(usuarioSession.getUsuLogin().getIdUsuarios()));
+            citas.setServiciosIdservicio(serviciosFacadeLocal.find(citas.getServiciosIdservicio().getIdservicio()));
+            citas.setTipopagoidTipoPago(tipoPagoFacadeLocal.find(citas.getTipopagoidTipoPago().getIdTipoPago()));
+
+            citasFacadeLocal.create(citas);
+
+            listacitas.clear();
+            listacitas.addAll(citasFacadeLocal.listaCitasPer(usuarioSession.getCedula()));
+
+            System.out.println("Se insertaron los datos correspondientes");
+
+        } catch (Exception e) {
+            System.out.println("Error al crear: " + e.getMessage());
+        }
+
+    }
+
+    public void eliminarCitaCli(Citas uscita) {
+
+        try {
+            citasFacadeLocal.remove(uscita);
+            listacitas.clear();
+            listacitas.addAll(citasFacadeLocal.listaCitasPer(usuarioSession.getCedula()));
+            System.out.println("Creado correctamente : ");
+        } catch (Exception e) {
+            System.out.println("Error al eliminar datos: " + e.getMessage());
+        }
+
+    }
+
+    public void cargadatoscita(Citas citap) {
+
+        try {
+            FacesContext fc = FacesContext.getCurrentInstance();
+            fc.getExternalContext().redirect("index_cita_update.xhtml");
+            this.citas = citap;
+            //../ClienteUsuario/index.xhtml
+        } catch (Exception e) {
+            System.out.println("Error al cargardatos: " + e);
+        }
+
+    }
+
+    public void actualizarCitaCli() {
+        try {
+            citas.setUsuariosidUsuarios(usuariosFacadeLocal.find(usuarioSession.getUsuLogin().getIdUsuarios()));
+            citas.setServiciosIdservicio(serviciosFacadeLocal.find(citas.getServiciosIdservicio().getIdservicio()));
+            citas.setTipopagoidTipoPago(tipoPagoFacadeLocal.find(citas.getTipopagoidTipoPago().getIdTipoPago()));
+            citasFacadeLocal.edit(citas);
+        } catch (Exception e) {
+            System.out.println("Error al actualizar cita: " + e);
+        }
+    }
+
     public CitaClienteView() {
     }
 
@@ -77,5 +153,53 @@ public class CitaClienteView implements Serializable{
     public void setListacitas(ArrayList<Citas> listacitas) {
         this.listacitas = listacitas;
     }
-    
+
+    public TipoPago getTipoPago() {
+        return tipoPago;
+    }
+
+    public void setTipoPago(TipoPago tipoPago) {
+        this.tipoPago = tipoPago;
+    }
+
+    public Usuarios getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(Usuarios usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public Servicios getServicios() {
+        return servicios;
+    }
+
+    public void setServicios(Servicios servicios) {
+        this.servicios = servicios;
+    }
+
+    public ArrayList<TipoPago> getListapago() {
+        return listapago;
+    }
+
+    public void setListapago(ArrayList<TipoPago> listapago) {
+        this.listapago = listapago;
+    }
+
+    public ArrayList<Usuarios> getListausu() {
+        return listausu;
+    }
+
+    public void setListausu(ArrayList<Usuarios> listausu) {
+        this.listausu = listausu;
+    }
+
+    public ArrayList<Servicios> getListaservices() {
+        return listaservices;
+    }
+
+    public void setListaservices(ArrayList<Servicios> listaservices) {
+        this.listaservices = listaservices;
+    }
+
 }
